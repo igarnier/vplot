716aae5bf6d2e2adfd7433c59210d094e4b968b3
Ilias <ilias.gar@gmail.com>
2017-08-27 10:49:06 +0200

    moved heatmap to its own file, cleanup a bit

--- lib/plot.ml
+++ lib/plot.ml
@@ -1,6 +1,7 @@
 open Vlayout
 open Tsdl
 
+(* Instantiate the Vlayout.Commands functor *)
 module Name =
   struct 
     type t = int
@@ -11,16 +12,13 @@ module Name =
       else -1
 
     let print = string_of_int
-   
+                  
   end
-
+    
 module Commands = Commands.Make(Name)
 
 module Backend = Backends.Cairo(Commands)
 
-
-
-type color  = float * float * float
 
 type domain = (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Array1.t
 
@@ -34,234 +32,34 @@ type target =
                                
 type window = Tsdl.Sdl.surface
 
-type plot = Commands.layout
-                               
-type range = { low : float; high : float }
+let init_timer () =
+  match Sdl.init Sdl.Init.timer with
+  | Error (`Msg msg) -> Sdl.log "Timer init error: %s" msg; exit 1
+  | Ok () -> ()
 
-let range_delta { low; high } = high -. low
-                               
-let interpolate a b n =
-  let d = (b -. a) /. (float n) in
-  let rec loop x i =
-    if i = n then
-      [b]
-    else
-      x :: (loop (x +. d) (i+1))
-  in
-  loop a 0
-
-let font_width     = 10
-let tick_length    = ~-. 10.0
-let xlabel_to_tick = 10.0
-let ylabel_to_tick = 10.0
-
-let float_to_string f = Printf.sprintf "%.1f" f
-
-let axis ~xrange ~yrange ~xticks ~yticks ~xlength ~ylength ~clr ~data =
-  let ticks_x = interpolate xrange.low xrange.high xticks in
-  let ticks_y = interpolate yrange.low yrange.high yticks in
-  let pos_x   = interpolate 0.0 xlength xticks in
-  let pos_y   = interpolate 0.0 ylength yticks in
-  (* produce commands *)
-  Commands.(
-    let orig       = Pt.zero in  
-    let x_segment  = segment ~p1:orig ~p2:(Pt.pt xlength 0.0) in
-    let y_segment  = segment ~p1:orig ~p2:(Pt.pt 0.0 ylength) in
-    let xtick_cmds = List.fold_left2 (fun acc xpos xlab ->
-                         let tick = segment
-                                     ~p1:(Pt.pt xpos 0.0)
-                                     ~p2:(Pt.pt xpos tick_length) in
-                         let labl = text
-                                      ~pos:({ pos = Pt.pt xpos (~-. xlabel_to_tick); relpos = North })
-                                      ~size:10.0
-                                      ~text:(float_to_string xlab)
-                         in
-                         tick :: labl :: acc
-                       ) [] pos_x ticks_x
-    in
-    let ytick_cmds = List.fold_left2 (fun acc ypos ylab ->
-                         let tick = segment
-                                     ~p1:(Pt.pt 0.0 ypos)
-                                     ~p2:(Pt.pt tick_length ypos) in
-                         let labl = text
-                                      ~pos:({ pos = Pt.pt (~-. ylabel_to_tick) ypos; relpos = East })
-                                      ~size:10.0
-                                      ~text:(float_to_string ylab)
-                         in
-                         tick :: labl :: acc
-                       ) [] pos_y ticks_y
-    in
-    let (r, g, b) = clr in
-    cmd ~name:None
-        ([ style
-             ~style:Style.(make ~stroke:(solid_stroke ~clr:(rgb r g b)) ~fill:None)
-             ~subcommands:
-             ([ x_segment; y_segment ] @ xtick_cmds @ ytick_cmds)
-         ] @ data)
-  )
-
-
-(* let plot_image = Image.create 500 500 *)
-
-(* let _ = *)
-(*   for i = 0 to 500 - 1 do *)
-(*     for j = 0 to 500 - 1 do *)
-(*       Image.set plot_image i j 0xFFFF0000l *)
-(*     done *)
-(*   done *)
-          
-(* let im  = Commands.image ~pos:Pt.zero ~image:plot_image *)
-
-(* (\* The stuff we want to draw *\)           *)
-(* let layout = *)
-(*   axis *)
-(*     ~xrange:{ low = -. 10.0; high = 10.0 } *)
-(*     ~yrange:{ low = -. 10.0; high = 10.0 } *)
-(*     ~xticks:6 *)
-(*     ~yticks:6 *)
-(*     ~xlength:500.0 *)
-(*     ~ylength:500.0 *)
-(*     ~clr:(1.0, 1.0, 1.0) *)
-(*     ~data:[im] *)
-
-(* let layout = *)
-(*   Commands.hbox ~deltax:10.0 ~layout_list:[layout; layout] *)
-
-module Heatmap =
-  struct
-
-    let pi   = acos (~-. 1.0)
-    let tpi  = 2.0 /. pi
-
-    (* atan is bounded by pi/2 *)
-
-    let rgb r g b =
-      Int32.(logor (logor (shift_left r 16) (shift_left g 8)) b)
-
-    let interpolate (r1,g1,b1) (r2,g2,b2) f =
-      let deltar = float (r2 - r1) in
-      let deltag = float (g2 - g1) in
-      let deltab = float (b2 - b1) in
-      let r      = Int32.of_float (f *. deltar +. (float r1))
-      and g      = Int32.of_float (f *. deltag +. (float g1))
-      and b      = Int32.of_float (f *. deltab +. (float b1)) in
-      rgb r g b
-
-    (* Note that the palette is skewed towards low values. *)
-    let palette =
-      let palette = Array.make 256 0x00000000l in
-      let x1      = 85.0 in
-      let x2      = 85.0 *. 2.0 in
-      for i = 0 to (int_of_float x1) - 1 do
-        let ratio = (float i) /. x1 in
-        palette.(i) <- interpolate (0, 0, 0) (255, 0, 0) ratio
-      done;
-      for i = (int_of_float x1) to (int_of_float x2) - 1 do
-        let ratio = (float i -. x1) /. (x2 -. x1) in
-        palette.(i) <- interpolate (255, 0, 0) (0, 255, 0) ratio
-      done;
-      for i = (int_of_float x2) to 255 do
-        let ratio = (float i -. x2) /. (256.0 -. x2) in
-        palette.(i) <- interpolate (0, 255, 255) (0, 0, 255) ratio
-      done;
-      palette
-
-    let min : float -> float -> float =
-      fun x y ->
-      if x < y then x else y
-
-    let max : float -> float -> float =
-      fun x y ->
-      if x > y then x else y
-        
-    let float_to_color minv maxv f =
-      if f > 0.0 then
-        let f = min maxv f in
-        let f = max minv f in
-        let f = (f -. minv) /. (maxv -. minv) in
-        let r = Int32.of_float (f *. 255.0) in
-        rgb r 0l 0l
-      else
-        let f = -. f in
-        let f = min maxv f in
-        let f = max minv f in
-        let f = (f -. minv) /. (maxv -. minv) in
-        let b = Int32.of_float (f *. 255.0) in
-        rgb 0l 0l b
-
-    open Bigarray
-             
-    type 'a data = (float, 'a, c_layout) Array2.t
-
-    type 'a ticks = (float, 'a, c_layout) Array1.t
-
-    let heatmap ~frame_clr ~minv ~maxv ~xticks ~yticks ~xdomain ~ydomain ~data =
-      let xlength    = Array1.dim xdomain in
-      let ylength    = Array1.dim ydomain in
-      let xdata      = Array2.dim1 data in
-      let ydata      = Array2.dim2 data in
-      if xlength <> xdata || ylength <> ydata then
-        failwith (Sdl.log "invalid heatmap size: |xdomain| = %d, |ydomain| = %d, |data| = %dx%d" xlength ylength xdata ydata; exit 1)
-      else ();
-      (* get min/max value in data *)
-      (* let minv = ref max_float in *)
-      (* let maxv = ref (-. max_float) in *)
-      (* for i = 0 to xlength - 1 do *)
-      (*   for j = 0 to ylength - 1 do *)
-      (*     let f = data.{i, j} in *)
-      (*     minv := min !minv f; *)
-      (*     maxv := max !maxv f *)
-      (*   done *)
-      (* done; *)
-      (* let minv       = !minv in *)
-      (* let maxv       = !maxv in *)
-      (* let idelta     = 1.0 /. (maxv -. minv) in *)
-      let xrange     = { low = xdomain.{0}; high = xdomain.{xlength - 1} } in
-      let yrange     = { low = ydomain.{0}; high = ydomain.{ylength - 1} } in
-      let plot_image = Image.create xlength ylength in
-      for i = 0 to xlength - 1 do
-        for j = 0 to ylength - 1 do
-          let f = data.{i, j} in
-          let c = float_to_color minv maxv f in
-          Image.set plot_image i j c
-        done
-      done;
-      let image      = Commands.image ~pos:(Pt.pt 1.0 1.0) ~image:plot_image in
-      axis
-        ~xrange ~yrange
-        ~xticks ~yticks
-        ~xlength:(float xlength) ~ylength:(float ylength)
-        ~clr:frame_clr
-        ~data:[image]
-        
-  end
-
-(* let init () = *)
-(*   match Sdl.init Sdl.Init.video with *)
-(*   | Error (`Msg msg) -> *)
-(*      Sdl.log "Init error: %s" msg; exit 1 *)
-(*   | Ok () -> *)
-(*      () *)
-
+let init_video () =
+  match Sdl.init Sdl.Init.video with
+  | Error (`Msg msg) -> Sdl.log "Video init error: %s" msg; exit 1
+  | Ok () -> ()
+               
 let init_sdl  =
   let default_width = 10
   and default_height = 10 in
   fun () ->
-  (* Init SDL video *) 
-  match Sdl.init Sdl.Init.video with
-  | Error (`Msg msg) -> Sdl.log "Init error: %s" msg; exit 1
-  | Ok () ->
-     let window_result =
-       Sdl.create_window
-         ~w:default_width
-         ~h:default_height
-         "SDL OpenGL"
-         Sdl.Window.opengl
-     in
-     match window_result with
-     | Error (`Msg msg) -> Sdl.log "Create window error: %s" msg; exit 1
-     | Ok window ->
-        Sdl { window }
+  init_video ();
+  init_timer ();
+  let window_result =
+    Sdl.create_window
+      ~w:default_width
+      ~h:default_height
+      "SDL OpenGL"
+      Sdl.Window.opengl
+  in
+  match window_result with
+  | Error (`Msg msg) -> Sdl.log "Create window error: %s" msg; exit 1
+  | Ok window ->
+     Sdl.set_window_position window ~x:10 ~y:10;
+     Sdl { window }
 
 let init_pdf filename = Pdf { filename }
             
@@ -384,59 +182,4 @@ let display ~target ~plot =
   match target with
   | Sdl { window }   -> display_sdl window plot
   | Pdf { filename } -> display_pdf filename plot
-
                
-(* let window = init ~window_width:1080 ~window_height:1080 *)
-
-(* let _ = display window layout *)
-                  
-(* let _ = *)
-(*   Sdl.delay 2000l *)
-               
-
-    
-(* let cairo_pdf_init width height outfile = *)
-(*   let srf  = *)
-(*     Cairo.PDF.create *)
-(*       ~fname:outfile *)
-(*       ~width:(float width) *)
-(*       ~height:(float height) *)
-(*   in *)
-(*   let ctx  = Cairo.create srf  in *)
-(*   let _    = *)
-(*     Cairo.set_matrix *)
-(*       ctx *)
-(*       Cairo.({ *)
-(*                 xx = 1.0 ; yx = 0.0 ; *)
-(*                 xy = 0.0 ; yy = ~-. 1.0 ; *)
-(*                 x0 = 0.0 ; y0 = (float height) *)
-(*               }) *)
-(*   in *)
-(*   let _   = Cairo.set_line_width ctx 1.0 in *)
-(*   let _   = Cairo.select_font_face ctx "DejaVuSansMono" ~slant:Cairo.Upright ~weight:Cairo.Normal in *)
-(*   (ctx, srf) *)
-
-(* let render_layout_centered ctx xmargin ymargin layout = *)
-(*   let (cmds, bbox) = Commands.emit_commands_with_bbox layout in *)
-(*   let bbox = Commands.Bbox.of_commands cmds in *)
-(*   let w    = xmargin +. (Bbox.width bbox) *)
-(*   and h    = ymargin +. (Bbox.height bbox) in *)
-(*   let cmds = Commands.center_to_page (w, h) cmds in *)
-(*   let _    = *)
-(*     Cairo.set_matrix *)
-(*       ctx *)
-(*       Cairo.({ *)
-(*                 xx = 1.0 ; yx = 0.0 ; *)
-(*                 xy = 0.0 ; yy = ~-. 1.0 ; *)
-(*                 x0 = 0.0 ; y0 = h *)
-(*               }) *)
-(*   in *)
-(*   List.iter (Backend.render ctx) cmds; *)
-(*   Cairo.show_page ctx   *)
-
-
-(* let (context, surface) = cairo_init 500 500 "out.pdf" *)
-
-(* let () = *)
-(*   render_layout_centered surface context 1.0 1.0 layout; *)
-(*   Cairo.Surface.finish surface *)
