c5697ecff6ed58f306400f2bd71b9bd51833bfce
Ilias <ilias.gar@gmail.com>
2017-09-10 12:15:57 +0200

    Some helper functions

---
+++ lib/utils.ml
@@ -0,0 +1,51 @@
+open Bigarray
+
+module Log = Log.Make(struct let section = "utils" end)
+                     
+type float_ref = { mutable x : float }
+                   
+let min (f : float) (f' : float) =
+  if f < f' then f
+  else f'
+
+let max (f : float) (f' : float) =
+  if f < f' then f'
+  else f
+
+let vector_range (data : Plot.data2d) =
+  let len   = Array2.dim2 data in
+  let minv  = { x = max_float } in
+  let maxv  = { x = (-. max_float) } in
+  for i = 0 to len -1 do
+    let v = data.{0,i} in
+    minv.x <- min minv.x v;
+    maxv.x <- max maxv.x v
+  done;
+  (minv.x, maxv.x)
+    
+let data_range (data : Plot.data2d) =
+  let xdata = Array2.dim1 data in
+  let ydata = Array2.dim2 data in
+  let minv  = { x = max_float } in
+  let maxv  = { x = (-. max_float) } in
+  for i = 0 to xdata -1 do
+    for j = 0 to ydata -1 do
+      let v = data.{i, j} in
+      minv.x <- min minv.x v;
+      maxv.x <- max maxv.x v
+    done
+  done;
+  (minv.x, maxv.x)
+
+let interpolate a b n =
+  if n < 2 then (Log.error "interpolate: not enough interpolation steps"; exit 1);
+  let d = (b -. a) /. (float (n-1)) in
+  let rec loop x i =
+    if i = (n-1) then
+      [b]
+    else
+      x :: (loop (x +. d) (i+1))
+  in
+  loop a 0
+       
+let (++) f g x = g (f x)
